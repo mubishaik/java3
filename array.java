10e86d09936a8f3e5ec466c0fe06ad32b7c16f2b
mubi <smubeenanjum@gmail.com>
2023-06-10 18:51:07 +0530

    added : array.java

---
+++ array.java
@@ -0,0 +1,13 @@
+package array;
+import java.util.*;
+public class array {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int[] numbers = {1,5,6};
+	int i=0;
+	
+for(i=0;i<=numbers.length-1;i++) {
+		System.out.println(numbers[i]); }
+
+}
+}
