85b372308607f8701d254ee9aaeee73cbb5f2e8c
mubi <smubeenanjum@gmail.com>
2023-06-10 18:58:03 +0530

    added : reverseorderarray.java

---
+++ reverseorderarray.java
@@ -0,0 +1,13 @@
+package array;
+
+public class reverseorderarray {
+	public static void main(String[] args) {
+		
+	
+	int[] arr = new int[] {2,3,9,6,5,4,6};
+	for(int i=6;i>=0;i--) {
+		System.out.println(arr[i]);
+		
+	}
+}
+}
